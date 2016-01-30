6a8b57612956f8b3342d20667e7e457d81eff7e2
ricpacca <ricpacca@gmail.com>
2016-01-31 00:33:52 +0100

    Showing google maps position

--- app/src/main/java/uk/orgen/doughnut/MapsActivity.java
+++ app/src/main/java/uk/orgen/doughnut/MapsActivity.java
@@ -1,29 +1,71 @@
 package uk.orgen.doughnut;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IARegion;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,IALocationListener {
 
-    private GoogleMap mMap;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private Marker mMarker;
+    private IALocationManager mIALocationManager;
+
+    String TAG = "MapsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mIALocationManager = IALocationManager.create(this);
+
+        String floorPlanId = "my-floor-plan-id";
+        if (!TextUtils.isEmpty(floorPlanId)) {
+            final IALocation FLOOR_PLAN_ID = IALocation.from(IARegion.floorPlan(floorPlanId));
+            mIALocationManager.setLocation(FLOOR_PLAN_ID);
+        }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mIALocationManager.requestLocationUpdates(IALocationRequest.create(), this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mIALocationManager.removeLocationUpdates(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mIALocationManager.destroy();
+        super.onDestroy();
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +80,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        mMap.setMyLocationEnabled(true);
+
+        /* Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)); */
+    }
+
+    public void onLocationChanged(IALocation location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        if (mMarker == null) {
+            if (mMap != null) {
+                mMarker = mMap.addMarker(new MarkerOptions().position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
+            }
+        } else {
+            mMarker.setPosition(latLng);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // N/A
     }
 }
