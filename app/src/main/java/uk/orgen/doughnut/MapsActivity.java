266ed97a29d5e33db3adccaa95c8879515d42374
ricpacca <ricpacca@gmail.com>
2016-01-31 08:53:19 +0100

    Morning

--- app/src/main/java/uk/orgen/doughnut/MapsActivity.java
+++ app/src/main/java/uk/orgen/doughnut/MapsActivity.java
@@ -1,12 +1,23 @@
 package uk.orgen.doughnut;
 
 import android.Manifest;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.PointF;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,14 +33,49 @@ import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
 import com.indooratlas.android.sdk.IARegion;
 import com.firebase.client.Firebase;
+import com.indooratlas.android.sdk.resources.IAFloorPlan;
+import com.indooratlas.android.sdk.resources.IALatLng;
+import com.indooratlas.android.sdk.resources.IALocationListenerSupport;
+import com.indooratlas.android.sdk.resources.IAResourceManager;
+import com.indooratlas.android.sdk.resources.IAResult;
+import com.indooratlas.android.sdk.resources.IAResultCallback;
+import com.indooratlas.android.sdk.resources.IATask;
+import com.squareup.picasso.Picasso;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,IALocationListener {
+import java.io.File;
+import java.util.Map;
+
+public class MapsActivity extends FragmentActivity implements IALocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Marker mMarker;
     private IALocationManager mIALocationManager;
+    private IAResourceManager mFloorPlanManager;
+    private IATask<IAFloorPlan> mPendingAsyncResult;
+    private IAFloorPlan mFloorPlan;
+    private long mDownloadId;
+    private DownloadManager mDownloadManager;
 
     String TAG = "MapsActivity";
+
+
+    private IARegion.Listener mRegionListener = new IARegion.Listener() {
+
+        @Override
+        public void onEnterRegion(IARegion region) {
+            if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+                String id = region.getId();
+                Log.d(TAG, "floorPlan changed to " + id);
+                Toast.makeText(MapsActivity.this, id, Toast.LENGTH_SHORT).show();
+                fetchFloorPlan(id);
+            }
+        }
+
+        @Override
+        public void onExitRegion(IARegion region) {
+            // leaving a previously entered region
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +83,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
 
         mIALocationManager = IALocationManager.create(this);
+        mFloorPlanManager = IAResourceManager.create(this);
 
         String floorPlanId = "my-floor-plan-id";
         if (!TextUtils.isEmpty(floorPlanId)) {
@@ -54,6 +110,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onResume() {
         super.onResume();
         mIALocationManager.requestLocationUpdates(IALocationRequest.create(), this);
+        mIALocationManager.registerRegionListener(mRegionListener);
 
 		Firebase.setAndroidContext(this);
         Firebase myFirebaseRef = new Firebase("https://donat.firebaseio.com/");
@@ -63,7 +120,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onPause() {
         super.onPause();
+
         mIALocationManager.removeLocationUpdates(this);
+        mIALocationManager.unregisterRegionListener(mRegionListener);
     }
 
     @Override
@@ -72,53 +131,106 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onDestroy();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-
-        mMap.setMyLocationEnabled(true);
-
-        /* Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)); */
-    }
-
     public void onLocationChanged(IALocation location) {
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        if (mMarker == null) {
-            if (mMap != null) {
-                mMarker = mMap.addMarker(new MarkerOptions().position(latLng)
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
-            }
-        } else {
-            mMarker.setPosition(latLng);
+        if(mMarker != null) {
+            mMarker.remove();
         }
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        mMarker = mMap.addMarker(new MarkerOptions().position(latLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // N/A
+    }
+
+    /*  Broadcast receiver for floor plan image download */
+    private BroadcastReceiver onComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0L);
+            if (id != mDownloadId) {
+                Log.w(TAG, "Ignore unrelated download");
+                return;
+            }
+            Log.w(TAG, "Image download completed");
+            Bundle extras = intent.getExtras();
+            DownloadManager.Query q = new DownloadManager.Query();
+            q.setFilterById(extras.getLong(DownloadManager.EXTRA_DOWNLOAD_ID));
+            Cursor c = mDownloadManager.query(q);
+
+            if (c.moveToFirst()) {
+                int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                    // process download
+                    String filePath = c.getString(c.getColumnIndex(
+                            DownloadManager.COLUMN_LOCAL_FILENAME));
+                    showFloorPlanImage(filePath);
+                }
+            }
+            c.close();
+        }
+    };
+
+    private void showFloorPlanImage(String filePath) {
+        Log.w(TAG, "showFloorPlanImage: " + filePath);
+    }
+
+    /**
+     * Fetches floor plan data from IndoorAtlas server. Some room for cleaning up!!
+     */
+    private void fetchFloorPlan(String id) {
+        cancelPendingNetworkCalls();
+        final IATask<IAFloorPlan> asyncResult = mFloorPlanManager.fetchFloorPlanWithId(id);
+        mPendingAsyncResult = asyncResult;
+        if (mPendingAsyncResult != null) {
+            mPendingAsyncResult.setCallback(new IAResultCallback<IAFloorPlan>() {
+                @Override
+                public void onResult(IAResult<IAFloorPlan> result) {
+                    Log.d(TAG, "fetch floor plan result:" + result);
+                    if (result.isSuccess() && result.getResult() != null) {
+                        mFloorPlan = result.getResult();
+                        String fileName = mFloorPlan.getId() + ".img";
+                        String filePath = Environment.getExternalStorageDirectory() + "/"
+                                + Environment.DIRECTORY_DOWNLOADS + "/" + fileName;
+                        File file = new File(filePath);
+                        if (!file.exists()) {
+                            DownloadManager.Request request =
+                                    new DownloadManager.Request(Uri.parse(mFloorPlan.getUrl()));
+                            request.setDescription("IndoorAtlas floor plan");
+                            request.setTitle("Floor plan");
+                            // requires android 3.2 or later to compile
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.
+                                        Request.VISIBILITY_HIDDEN);
+                            }
+                            request.setDestinationInExternalPublicDir(Environment.
+                                    DIRECTORY_DOWNLOADS, fileName);
+
+                            mDownloadId = mDownloadManager.enqueue(request);
+                        } else {
+                            showFloorPlanImage(filePath);
+                        }
+                    } else {
+                        // do something with error
+                        if (!asyncResult.isCancelled()) {
+                            Toast.makeText(MapsActivity.this,
+                                    (result.getError() != null
+                                            ? "error loading floor plan: " + result.getError()
+                                            : "access to floor plan denied"), Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                }
+            }, Looper.getMainLooper()); // deliver callbacks in main thread
+        }
+    }
+
+    private void cancelPendingNetworkCalls() {
+        if (mPendingAsyncResult != null && !mPendingAsyncResult.isCancelled()) {
+            mPendingAsyncResult.cancel();
+        }
     }
 }
