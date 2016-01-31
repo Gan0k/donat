e854a836dd9824281d7ca78824d57ab6129df0bf
Guido Arnau <guido.arnau@gmail.com>
2016-01-31 07:51:52 +0000

    Add firebase support

--- app/src/main/java/uk/orgen/doughnut/MapsActivity.java
+++ app/src/main/java/uk/orgen/doughnut/MapsActivity.java
@@ -1,5 +1,9 @@
 package uk.orgen.doughnut;
 
+import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
+
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
@@ -7,6 +11,9 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.support.v4.app.FragmentActivity; import android.os.Bundle;
+import android.provider.Settings.Secure;
+import android.os.Handler;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,12 +29,19 @@ import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
 import com.indooratlas.android.sdk.IARegion;
 import com.firebase.client.Firebase;
+import com.firebase.client.ValueEventListener;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.DataSnapshot;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,IALocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Marker mMarker;
     private IALocationManager mIALocationManager;
+    private Firebase fireRef;
+    private String android_id;
+    private Runnable runnable;
+    private Handler handler;
 
     String TAG = "MapsActivity";
 
@@ -48,42 +62,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             final IALocation FLOOR_PLAN_ID = IALocation.from(IARegion.floorPlan(floorPlanId));
             mIALocationManager.setLocation(FLOOR_PLAN_ID);
         }
+
+		Firebase.setAndroidContext(this);
+        fireRef = new Firebase("https://donat.firebaseio.com/");
+        android_id = Secure.getString(getContentResolver(), Secure.ANDROID_ID);
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Random r = new Random();
+                Map<String, Double> pos = new HashMap<String, Double>();
+                pos.put("x", new Double(r.nextDouble()));
+                pos.put("y", new Double(r.nextDouble()));
+                fireRef.child(android_id).setValue(pos);
+                handler.postDelayed(this, 1000);
+            }
+        };
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mIALocationManager.requestLocationUpdates(IALocationRequest.create(), this);
-
-		Firebase.setAndroidContext(this);
-        Firebase myFirebaseRef = new Firebase("https://donat.firebaseio.com/");
-        myFirebaseRef.child("message").setValue("BIENE ALESSIO!");
+        handler.postDelayed(runnable,1000);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         mIALocationManager.removeLocationUpdates(this);
+        handler.removeCallbacks(runnable);
     }
 
     @Override
     protected void onDestroy() {
         mIALocationManager.destroy();
+        handler.removeCallbacks(runnable);
         super.onDestroy();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        // Add listener to firebase ref
+        fireRef.addValueEventListener(new ValueEventListener() {
+              @Override
+              public void onDataChange(DataSnapshot snapshot) {
+                  System.out.println("There are " + snapshot.getChildrenCount() + " people connected");
+                  for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                      Double x = (Double) postSnapshot.child("x").getValue();
+                      Double y = (Double) postSnapshot.child("y").getValue();
+                      System.out.println(String.valueOf(x) + " - " + String.valueOf(y));
+                  }
+              }
+              @Override
+              public void onCancelled(FirebaseError firebaseError) {
+                  System.out.println("The read failed: " + firebaseError.getMessage());
+              }
+        });
+
+        // Start handler to broadcast position
+        handler.postDelayed(runnable, 1000);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -121,4 +159,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // N/A
     }
+
 }
+
